d69d2819909ebbf8f0fec3232d2112ff6467078b
munisa-analyst <munisamuhammadiyeva0@gmail.com>
2026-02-21 04:05:10 +1000

    Update variables.py

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -1,1 +1,50 @@
+1-task
+ism="Anvar"
+yosh=22
+balandlik= 171.3
+faol= True
+print(ism,yosh,balandlik,faol)
 
+2-task
+ism=input("ismingizni kiriting: " )
+yosh=int(input("yoshingizni kiriting: " ))
+fan=input("sevimli fanizni kiriting: " )
+print("Salom, mening ismim", ism )
+print("Yoshim", yosh, "da")
+print("Sevimli fanim", fan)
+
+3-task
+ism=input("ismingizni kiriting: ")
+yosh=int(input("yoshingizni kiriting: "))
+rang=input("sevimli rangingizni kiriting: ")
+print("Salom,mening ismim", ism, "yoshim", yosh, "da","va sevimli rangim", rang)
+
+# 4-task
+a=int(input("Birinchi sonni kiriting:"))
+b=int(input("Ikkinchi sonni kiriting:"))
+print("Yig'indisi:", a+b)
+print("Ko'paytmasi:", a*b)
+
+5-task
+ism=input("ismingizni kiriting: ")
+familiya=input("familiyangizni kiriting: ")
+yosh=int(input("yoshingizni kiriting: "))
+print(ism,familiya,yosh)
+
+6-task
+son=int(input("son kiriting:"))
+print("2 ga bo'lish:", son//2)
+print("2 ga ko'paytirish:", son*2)
+
+7-task
+taom=input("sevimli taomingizni kiriting:")
+ichimlik=input("sevimli ichimlikingizni kiriting:")
+print("Sevimli taomim:", taom, "va sevimli ichimligim:", ichimlik)
+
+8-task
+a=int(input("Birinchi sonni kiriting:"))
+b=int(input("Ikkinchi sonni kiriting:"))
+print("Yig'indisi:", a+b)
+print("Ayirmasi:", a-b)
+print("Ko'paytmasi:", a*b)
+print("Bo'linmasi:", a/b)
